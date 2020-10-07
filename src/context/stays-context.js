ae8e6260865179815b1d024b1e31a59ef7c5f8fa
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-10-07 23:29:29 +0700

    add action filter data and toggle filter on recuder

--- src/context/stays-context.js
+++ src/context/stays-context.js
@@ -1,5 +1,6 @@
 import React from "react";
 import items from "../data/stays.json";
+import { FILTER_DATA, TOOGLE_FILTER } from "./action";
 
 export const StaysContext = React.createContext();
 
@@ -7,13 +8,37 @@ const initalState = {
   items,
   shortedItems: items,
   city: "",
+  child: 0,
+  adult: 0,
   guest: 0,
+
+  toggleFilter: false,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case FILTER_DATA:
+      const { location, adult, child, guest } = action;
+      let tempItems = [...state.items];
+      if (location !== "") {
+        tempItems = tempItems.filter((item) => item.city === location);
+      }
+      if (guest !== 0) {
+        tempItems = tempItems.filter((item) => guest <= item.maxGuests);
+      }
+
+      return {
+        ...state,
+        city: location,
+        adult: adult,
+        child: child,
+        guest: guest,
+        shortedItems: tempItems,
+      };
+    case TOOGLE_FILTER:
+      return { ...state, toggleFilter: !state.toggleFilter };
     default:
-      throw new Error();
+      return state;
   }
 };
 
